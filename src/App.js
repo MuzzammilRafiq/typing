9e296d22b6fa928978d1e02ab2f4bd0e514fa8ff
MuzzammilRafiq <mzmlrafiqcse@gmail.com>
2023-03-12 14:41:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [message, setMessage] = useState("");
+  const [errors, setErrors] = useState(0);
+  const [words, setwords] = useState(0);
+
+  const countWords = (str) => {
+    return str.replace(/-/g, " ").trim().split(/\s+/g).length;
+  };
+
+  const handleKeyDown = (event) => {
+    setwords(countWords(message));
+    if (event.key === "Backspace") {
+      setErrors(errors + 1);
+    }
+  };
+  const handleReset = () => {
+    setMessage("");
+    setErrors(0);
+    setwords(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "auto", width: "70%", textAlign: "center" }}>
+      <h1>Errors: {errors}</h1>
+      <h1>Words:{words}</h1>
+      <textarea
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}
+        onKeyDown={handleKeyDown}
+        rows={20}
+        cols={60}
+        style={{ resize: "none" }}
+      />
+      <br />
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
-
-export default App;
